ecf32dc2e51ed465c161b63c1aaf16d60920b220
vtu26619-ux <vtu26619@veltech.edu.in>
2026-02-13 11:10:39 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (12.94%) - LeetSync

---
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -0,0 +1,20 @@
+
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+
+        List<Integer> res = new ArrayList<>();
+
+        preorder(root, res);
+
+        return res;
+    }
+
+    private void preorder(TreeNode root, List<Integer> res) {
+
+        if(root == null) return ;
+
+        res.add(root.val);
+        preorder(root.left, res);
+        preorder(root.right, res);
+    }
+}
