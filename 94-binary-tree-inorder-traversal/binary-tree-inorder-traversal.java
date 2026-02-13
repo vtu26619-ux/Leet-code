ccb9d910aa38fdd40a88f02fb13388fbfa1467f7
vtu26619-ux <vtu26619@veltech.edu.in>
2026-02-13 10:26:31 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (69.23%) - LeetSync

---
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -0,0 +1,20 @@
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+        List<Integer> res = new ArrayList<>();
+
+        inorder(root, res);
+
+        return res;
+    }
+
+    private void inorder(TreeNode root, List<Integer> res) {
+        if(root == null) return;
+
+        inorder(root.left, res);
+        res.add(root.val);
+        inorder(root.right, res);
+    }
+}
+
